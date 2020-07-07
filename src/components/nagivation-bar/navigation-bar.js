a48e08c59a4bd962a5a35678ed44f963b6463c06
Liam Becker <liambecker28@gmail.com>
2020-07-07 11:54:41 -0400

    Create NavigationBar component

--- src/components/nagivation-bar/navigation-bar.js
+++ src/components/nagivation-bar/navigation-bar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import navigationStyles from './navigation-bar.module.css';
 
 const NavigationBar = () => {
     return (
         <div className={navigationStyles.container}>
-            <button className={navigationStyles.button}>Home</button>
-            <button className={navigationStyles.button}>Groceries</button>
-            <button className={navigationStyles.cart}>Cart</button>
+            <Button className={navigationStyles.button}>Home</Button>
+            <Button className={navigationStyles.button}>Groceries</Button>
+            <Button className={navigationStyles.cart}>Cart</Button>
         </div>
     );
 };
