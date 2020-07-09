8171e4c1f78a24c48a24835aff215e316e62d462
Liam Becker <liambecker28@gmail.com>
2020-07-09 16:39:58 -0400

    Add icons to navigation bar

--- src/components/page-layout/NavigationBar.js
+++ src/components/page-layout/NavigationBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 // Componenets
 import { Button, Grid } from '@material-ui/core';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import HomeIcon from '@material-ui/icons/Home';
 
 // Styles
 import styles from './styles/navigation-bar.module.css';
@@ -11,13 +14,18 @@ const NavigationBar = () => (
     <Grid container justify="space-between">
       <Grid item>
         <div>
-          <Button className={styles.button}>Home</Button>
-          <Button className={styles.button}>Groceries</Button>
+          <Button className={styles.button}>
+            <HomeIcon className={styles.icon} />
+          </Button>
+          <Button className={styles.button}>
+            <StorefrontIcon className={styles.icon} />
+          </Button>
         </div>
       </Grid>
-
       <Grid item>
-        <Button className={styles.cart}>Cart</Button>
+        <Button className={styles.button}>
+          <ShoppingBasketIcon className={styles.icon} />
+        </Button>{' '}
       </Grid>
     </Grid>
   </div>
