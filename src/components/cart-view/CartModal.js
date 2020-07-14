b19efe44f6e53d3552c88fde0978a56a387020ec
will <sopwill3@gmail.com>
2020-07-14 11:09:22 -0600

    added cart modal, added nav links

--- src/components/cart-view/CartModal.js
+++ src/components/cart-view/CartModal.js
@@ -1,25 +1,143 @@
 // import PropTypes from 'prop-types';
-// import React from 'react';
+import React from 'react';
 
-// // Components
-// import CartItem from './CartItem';
+// Components
+import {
+  Grid,
+  Drawer,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import CartItem from './CartItem';
 
-// // Styles
-// import styles from './styles/cart-modal.module.css';
+// Styles
+import styles from './styles/cart-modal.module.css';
 
-// const CartModal = () => (
-//     <div>
-//       <CartItem />
-//       <CartItem />
-//       <CartItem />
-//       <CartItem />
-//     </div>
-// );
+// Vars
 
-// CartModal.propTypes = {
-// };
+const dummyData = [
+  {
+    name: 'orange',
+    amount: 5,
+  },
+  {
+    name: 'peach',
+    amount: 3,
+  },
+  {
+    name: 'grape',
+    amount: 5,
+  },
+  {
+    name: 'beer',
+    amount: 1,
+  },
+  {
+    name: 'beef',
+    amount: 15,
+  },
+  {
+    name: 'Doritos',
+    amount: 2,
+  },
+  {
+    name: 'apples',
+    amount: 7,
+  },
+  {
+    name: 'orange',
+    amount: 5,
+  },
+  {
+    name: 'peach',
+    amount: 3,
+  },
+  {
+    name: 'grape',
+    amount: 5,
+  },
+  {
+    name: 'beer',
+    amount: 1,
+  },
+  {
+    name: 'beef',
+    amount: 15,
+  },
+  {
+    name: 'Doritos',
+    amount: 2,
+  },
+  {
+    name: 'apples',
+    amount: 7,
+  },
+];
 
-// CartModal.defaultProps = {
-// };
+const CartModal = ({ open, toggleModal }) => {
+  const generateItems = dummyData.map((item) => (
+    <CartItem name={item.name} amount={item.amount} />
+  ));
 
-// export default CartModal;
+  return (
+    <div>
+      <Drawer anchor='right' open={open} onClose={toggleModal}>
+        <Grid
+          container
+          className={styles.modalContentBox}
+          justifyContent='center'
+        >
+
+          <Grid
+            item
+            sm={12}
+          >
+            <Typography
+              variant='h2'
+              align='center'
+            >
+              Cart
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            sm={12}
+            className={styles.cartList}
+          >
+            {generateItems}
+          </Grid>
+
+          <Grid
+            item
+            sm={12}
+            className={styles.cartButtonBox}
+          >
+            <Button
+              href='/receipt'
+              variant="contained"
+              color="secondary"
+              className={styles.cartButton}
+            >
+              <Typography
+                variant='h5'
+                align='center'
+              >
+                Checkout
+              </Typography>
+            </Button>
+          </Grid>
+
+        </Grid>
+      </Drawer>
+    </div>
+  );
+};
+
+CartModal.propTypes = {
+};
+
+CartModal.defaultProps = {
+};
+
+export default CartModal;
