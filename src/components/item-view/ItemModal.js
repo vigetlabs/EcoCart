0856468dd73f3931e4c72ff5ae79f8ca80e9c1d7
Liam Becker <liambecker28@gmail.com>
2020-07-15 09:46:34 -0400

    Buildout info section of item modal

--- src/components/item-view/ItemModal.js
+++ src/components/item-view/ItemModal.js
@@ -1,29 +1,55 @@
 import React from 'react';
 
 // Components
-import { Modal, Card, CardContent } from '@material-ui/core';
+import { Modal, Paper, Grid } from '@material-ui/core';
+import InfoSection from './info-section/InfoSection';
+
+// Style
+import styles from './styles/item-modal.module.css';
+
+// Utils
+import { getUrl } from '../grocery-view/grocery-section/placeholder-data';
 
 const closeModal = (setModalOpen) => {
   setModalOpen(false);
 };
 
-const ItemModal = ({ isModalOpen, setModalOpen, item }) => {
-  const body = (
-    <div>
-      <h1>This is a modal!</h1>
-    </div>
-  );
-
-  return (
-    <Modal
-      open={isModalOpen}
-      onClose={() => closeModal(setModalOpen)}
-      aria-labelledby={`${item}-modal`}>
-      <Card>
-        <CardContent>{item}</CardContent>
-      </Card>
-    </Modal>
-  );
-};
+const ItemModal = ({ isModalOpen, setModalOpen, item }) => (
+  <Modal
+    open={isModalOpen}
+    onClose={() => closeModal(setModalOpen)}
+    aria-labelledby={`${item}-modal`}>
+    <Paper className={styles.modal}>
+      <Grid
+        container
+        direction="column"
+        alignItems="flex-end"
+        alignContent="space-around"
+        justify="center"
+        className={styles.grid}>
+        <Grid item>
+          <Grid container alignContent="space-around" justify="center">
+            <Grid item className={styles.square}>
+              <img className={styles.picture} src={getUrl(item)} alt={item} />
+            </Grid>
+            <Grid item className={styles.square}>
+              <InfoSection item={item} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container justify="center">
+            <Grid item className={styles.square}>
+              <h1>Donut!</h1>
+            </Grid>
+            <Grid item className={styles.square}>
+              <h1>Details!</h1>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  </Modal>
+);
 
 export default ItemModal;
