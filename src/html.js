d9c01bd606d3fbd0e98d07ab072ce661cc323802
will <sopwill3@gmail.com>
2020-07-27 11:55:13 -0600

    fixed google issues

--- src/html.js
+++ src/html.js
@@ -5,13 +5,15 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N6HVBGD');</script>`,
-        }} />
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6HVBGD');`,
+      }}></script>
+      {/* End Google Tag Manager */}
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -29,14 +31,16 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
       </body>
-      <noscript>
-        <iframe
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-N6HVBGD"
           height="0"
           width="0"
           style="display:none;visibility:hidden">
-        </iframe>
-      </noscript>
+        </iframe>`,
+      }}></noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
     </html>
   );
 }
