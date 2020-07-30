addcbac171cb24d02b7ad7371742af228c2e53c3
Liam Becker <liambecker28@gmail.com>
2020-07-30 09:26:52 -0400

    Revert font changes

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-// import './fonts.css';
+import './fonts.css';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // See https://material-ui.com/customization/globals/
@@ -6,45 +6,45 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 export default createMuiTheme({
   typography: {
     h1: {
-      // fontFamily: 'Rubik Mono One',
+      fontFamily: 'Rubik Mono One',
       fontSize: 50,
     },
     h2: {
-      // fontFamily: 'Karla',
+      fontFamily: 'Karla',
       fontSize: 50,
     },
     h3: {
-      // fontFamily: 'Karla',
+      fontFamily: 'Karla',
       fontSize: 40,
     },
-    // h4: {
-    //   fontFamily: 'Karla',
-    // },
-    // h5: {
-    //   fontFamily: 'Rubik',
-    // },
-    // h6: {
-    //   fontFamily: 'Rubik',
-    // },
+    h4: {
+      fontFamily: 'Karla',
+    },
+    h5: {
+      fontFamily: 'Rubik',
+    },
+    h6: {
+      fontFamily: 'Rubik',
+    },
     subtitle1: {
-      // fontFamily: 'Rubik',
+      fontFamily: 'Rubik',
       fontSize: 25,
     },
     subtitle2: {
-      // fontFamily: 'Rubik',
+      fontFamily: 'Rubik',
       fontSize: 25,
     },
     body1: {
-      // fontFamily: 'Rubik',
+      fontFamily: 'Rubik',
       fontSize: 20,
     },
-    // body2: {
-    //   fontFamily: 'Rubik',
-    // },
-    // button: {
-    //   fontFamily: 'Rubik',
-    // },
-    // fontFamily: '"Rubik", "Arial", sans-serif',
+    body2: {
+      fontFamily: 'Rubik',
+    },
+    button: {
+      fontFamily: 'Rubik',
+    },
+    fontFamily: '"Rubik", "Arial", sans-serif',
     fontWeightLight: 300,
     fontWeightRegular: 400,
     fontWeightMedium: 500,
