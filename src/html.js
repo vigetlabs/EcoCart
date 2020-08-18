be8721d389df4f2ebff03fb64df7cfbf19d437da
Ryan Schaefer <23317410+ryanschaefer41@users.noreply.github.com>
2020-08-18 13:56:15 -0400

    update metadata img URLs to point to S3 bucket

--- src/html.js
+++ src/html.js
@@ -28,11 +28,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:title' content='EcoCart | Calculate your Environmental FOODprint' />
         <meta name='twitter:description' content='How eco-friendly is the beef you eat? Bring your grocery list to life with an in-depth look at the environmental impact of each food item on your list! EcoCart is an educational shopping experience that gives you a personalized grade based on the environmental impact of your most common grocery items.' />
-        <meta name='twitter:image:src' content='https://lh3.googleusercontent.com/LiO4QtoXi5U8kvLe9cI3gd2HTF9OnKB_w5ETfYpuSzOCK3TAPg_BzMSZ8fDxLIkwMe8UlUMw7i5L5-CeI-KtI86WBnleauexj9q3AsT8FzFLYvvbE_GTpml9yM-TP6Cz8DaOWbdywQ' />
+        <meta name='twitter:image:src' content='https://ecocart.s3.amazonaws.com/hero.png' />
 
         <meta property="og:title" content="EcoCart | Calculate your Environmental FOODprint" />
         <meta property="og:url" content="https://ecocart.app/" />
-        <meta property="og:image" content="https://lh3.googleusercontent.com/LiO4QtoXi5U8kvLe9cI3gd2HTF9OnKB_w5ETfYpuSzOCK3TAPg_BzMSZ8fDxLIkwMe8UlUMw7i5L5-CeI-KtI86WBnleauexj9q3AsT8FzFLYvvbE_GTpml9yM-TP6Cz8DaOWbdywQ" />
+        <meta property="og:image" content="https://ecocart.s3.amazonaws.com/hero.png" />
         <meta property="og:description" content="How eco-friendly is the beef you eat? Bring your grocery list to life with an in-depth look at the environmental impact of each food item on your list! EcoCart is an educational shopping experience that gives you a personalized grade based on the environmental impact of your most common grocery items." />
         <meta property="og:site_name" content="EcoCart | Calculate your Environmental FOODprint" />
 
